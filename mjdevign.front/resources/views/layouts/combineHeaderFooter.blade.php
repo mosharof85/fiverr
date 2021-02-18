913905805ecb39486795122ab9133927c3d971f4
mosharof85 <mosharof.hossain@msn.commosharof85>
2021-02-18 18:55:39 +0600

    updated

--- mjdevign.front/resources/views/layouts/combineHeaderFooter.blade.php
+++ mjdevign.front/resources/views/layouts/combineHeaderFooter.blade.php
@@ -214,6 +214,31 @@
         </div>
     </div>
 </footer>
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+        });
+    };
+
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your Chat Plugin code -->
+<div class="fb-customerchat"
+     attribution="setup_tool"
+     page_id="105398154682392">
+</div>
+
+
 <script src="{{ asset ( 'main/js/header.js' ) }}"></script>
 <script src="{{ asset ( 'main/js/package-controller.js' ) }}"></script>
 <script src="{{ asset ( 'main/js/faq.js' ) }}"></script>
